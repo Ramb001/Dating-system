1f0833cf85d043677c33380a5c0af04439054372
Artem <artem.rmb@bk.ru>
2024-06-20 12:27:52 +0300

    -

--- backend/routers/data.py
+++ backend/routers/data.py
@@ -52,8 +52,8 @@ async def likes(user_id: str):
         likes = await PB.fetch_records(
             PocketbaseCollections.LIKES,
             client,
-            # filter=f"receiver='{user_id}'",
-            expand="interests",
+            filter=f"receiver='{user_id}'",
+            expand="sender,interests",
         )
 
         return likes["items"]
